ac4170963dcc6b10fd4295c60fbe6f43b5e41cb8
Brandon Moye <brandon.m.moye@gmail.com>
2023-11-04 14:31:16 -0500

    search box working

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-const url = 'https://whos-that-api.onrender.com/api/whosThatApi/Pokemon?pokemon=garchomp&password=mudkip-rules'
+const searchButton = document.getElementById('searchButton')
+const pokemonInput = document.getElementById('pokemonInput')
 
-
-fetch(url, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-
-.then(res => {
-    if (!res.ok) {
-        throw new Error('Network response was not okay')
-    }
-    return res.json()
-})
-.then(data => {
-    document.getElementById('pokemonImage').src = data['Pokemon Details']['pokemonImage']
-
-    function setTextContentById(id, value) {
-        const element = document.getElementById(id)
-        if (element && element !== 'pokemonImage') {
-            element.textContent = value;
+function searchForPokemon() {
+    const pokemonName = document.getElementById('pokemonInput').value;
+    const url = `https://whos-that-api.onrender.com/api/whosThatApi/Pokemon?pokemon=${pokemonName}&password=mudkip-rules`
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
         }
-    }
-
-    Object.keys(data['Pokemon Details']).forEach(key => {
-        const value = data['Pokemon Details'][key]
-        return setTextContentById(key, value)
-        
     })
     
-})
-.catch(error => {
-    return (error)
-})
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('Network response was not okay')
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById('pokemonImage').src = data['Pokemon Details']['pokemonImage']
+    
+        function setTextContentById(id, value) {
+            const element = document.getElementById(id)
+            if (element && element !== 'pokemonImage') {
+                element.textContent = value;
+            }
+        }
+    
+        Object.keys(data['Pokemon Details']).forEach(key => {
+            const value = data['Pokemon Details'][key]
+            return setTextContentById(key, value)
+            
+        })
 
+        console.log(data)
+        
+    })
+    .catch(error => {
+        return (error)
+    })
+    
+    pokemonInput.value = '';
+}
+
+searchButton.addEventListener('click', searchForPokemon)
+pokemonInput.addEventListener('keydown', e => {
+    if(e.key === 'Enter') {
+        searchForPokemon()
+    }
+})
