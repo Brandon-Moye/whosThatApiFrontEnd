bfd7f70a43ea7de8d7599110a9473757873426f8
Brandon Moye <brandon.m.moye@gmail.com>
2023-11-04 14:02:16 -0500

    function to display data

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-fetch('https://whos-that-api.onrender.com/api/whosThatApi/Pokemon?pokemon=annihilape&password=mudkip-rules')
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-    const outputElement = document.getElementById('pokemonData')
-    outputElement.textContent = JSON.stringify(data, null, 2)
-    pokemonNameOnDom = document.getElementById('pokemonName')
-    pokemonNationalDexNumOnDom = document.getElementById('pokemonNationalDexNum')
-    pokemonName.textContent = JSON.stringify(data["Pokemon Details"]["pokemonName"])
-    pokemonNationalDexNumOnDom.textContent = JSON.stringify(data["Pokemon Details"]["nationalDexNum"])
+const url = 'https://whos-that-api.onrender.com/api/whosThatApi/Pokemon?pokemon=garchomp&password=mudkip-rules'
+
+
+fetch(url, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
 })
+
+.then(res => {
+    if (!res.ok) {
+        throw new Error('Network response was not okay')
+    }
+    return res.json()
+})
+.then(data => {
+    document.getElementById('pokemonImage').src = data['Pokemon Details']['pokemonImage']
+
+    function setTextContentById(id, value) {
+        const element = document.getElementById(id)
+        if (element && element !== 'pokemonImage') {
+            element.textContent = value;
+        }
+    }
+
+    Object.keys(data['Pokemon Details']).forEach(key => {
+        const value = data['Pokemon Details'][key]
+        return setTextContentById(key, value)
+        
+    })
+    
+})
+.catch(error => {
+    return (error)
+})
+
